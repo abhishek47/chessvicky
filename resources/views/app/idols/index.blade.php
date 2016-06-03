e02c119e0b005f13348022c071bfbb2b9d3cc5d3
abhishek wani <abhishek wani>
2016-06-03 09:13:38 +0530

    Admin Complete

--- resources/views/app/idols/index.blade.php
+++ resources/views/app/idols/index.blade.php
@@ -13,12 +13,12 @@
                                 <img src="{{ $idol->photo_url }}" class="thumb-lg img-circle img-thumbnail" alt="img">
                                 <h4>{{ $idol->name }}</h4>
                                 <p class="text-muted"><i class="fa fa-user"></i> {{ $idol->user->username }}</p>
-                                <a href="#" class="btn btn-sm btn-purple m-t-20">View Conversations</a>
-                                <a href="#" class="btn btn-sm btn-success m-t-20">New Conversation</a>
+                                <a href="#" class="btn btn-sm btn-purple m-t-20">Follow</a>
+                                <a href="#" class="btn btn-sm btn-success m-t-20">My Conversations</a>
                                 <p class="m-t-10 text-muted p-20">{{ $idol->desc }}</p>
                                 <ul class="list-inline widget-list clearfix">
                                     <li class="col-md-4"><span>2.109</span>Followers</li>
-                                    <li class="col-md-4"><span>596</span>Photos</li>
+                                    <li class="col-md-4"><span>596</span>Conversations</li>
                                     <li class="col-md-4"><span>902</span>Like</li>
                                 </ul>
                             </div>
