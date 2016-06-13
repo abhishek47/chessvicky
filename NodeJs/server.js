d7d1642a870b347cca0b3d61137c731ceb39f6f6
abhishek wani <abhishek wani>
2016-06-13 14:12:02 +0530

    LiveGame

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
  
-server.listen(8080, '104.131.2.218');
+server.listen();
 
 console.log ("Node Server Started");
 
