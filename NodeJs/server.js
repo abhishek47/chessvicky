6a81c4007140af412e2134493996a6085c2e0bd1
abhishek wani <abhishek wani>
2016-06-13 15:58:38 +0530

    LiveGame

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,28 +1,28 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var redis = require('redis');
  
 server.listen(3000);
-
-console.log ("Node Server Started");
-
+console.log("Node Server Started");
 io.on('connection', function (socket) {
  
   console.log("new client connected");
- 
-  socket.on("join", function(room) {
-    socket.join(room);
-    console.log('user joined room: ' + room);
-  });
- 
-  socket.on("move", function(move) {
-    console.log('user moved: ' + JSON.stringify(move));
-    io.emit('move', move);
+   io.on('connection', function(socket){ //join room on connect
+    socket.on('join', function(room) {
+      socket.join(room);
+      console.log('user joined room: ' + room);
+    });
+    socket.on('move', function(move) { //move object emitter
+      console.log('user moved: ' + JSON.stringify(move));
+      io.emit('move', move);
+    });
   });
  
   socket.on('disconnect', function() {
-     console.log('user disconnected');
+   
   });
  
 });
+ 
+
