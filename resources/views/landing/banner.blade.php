b204362ae2a30e0bd1f978d601cd6e0e148f5637
abhishek wani <abhishek wani>
2016-07-21 11:22:53 +0530

    New Tagline

--- resources/views/landing/banner.blade.php
+++ resources/views/landing/banner.blade.php
@@ -5,7 +5,7 @@
 
 		   <div class="hero text-center">
 		   	   <h2>Embrace Yourself By Learning Chess With Us!!</h2>
-		   	   <h3>Let's Have One Last Move</h3>
+		   	   <h3>Be Always One Move Ahead</h3>
 
 		   	   <a href="/register?trial=true" class="btn btn-default green">Try It Free</a>
 		   	   <a rel="vimeo" href="http://vimeo.com/29193046" class="btn btn-default swipebox"><i class="fa fa-play-circle-o"></i> Know More</a>
