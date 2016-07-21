0b376700d463117977e78820ee1e2f73ea4766f4
abhishek wani <abhishek wani>
2016-07-21 11:41:34 +0530

    New Partner

--- resources/views/landing/banner.blade.php
+++ resources/views/landing/banner.blade.php
@@ -8,7 +8,7 @@
 		   	   <h3>Be Always One Move Ahead</h3>
 
 		   	   <a href="/register?trial=true" class="btn btn-default green">Try It Free</a>
-		   	   <a rel="vimeo" href="http://vimeo.com/29193046" class="btn btn-default swipebox"><i class="fa fa-play-circle-o"></i> Know More</a>
+		   	   <a rel="vimeo" href="http://vimeo.com/29193046" class="btn btn-default swipebox"><i class="fa fa-play-circle-o"></i> Watch Demo</a>
 		   </div>
 	</div>
 </div>
