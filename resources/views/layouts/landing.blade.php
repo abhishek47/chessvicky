005754d51e1df7f0f7e494720d4877b66e3004b3
abhishek wani <abhishek wani>
2016-08-20 15:40:41 +0530

    Akash Info

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -84,13 +84,8 @@
         <h4 class="modal-title" id="myModalLabel">Akash Developers</h4>
       </div>
       <div class="modal-body">
-        <h3>Real Estate Dealers and Land Developers</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h3>Real Estate and Land Developers</h3>
+        <p>Akash Developers is a leading real estate company in Nashik led by Mr. Pravin Wani and Akash Wani.Founded since 1982 Akash Developers has built various different Infrastructures.</p>
       </div>
       
     </div>
